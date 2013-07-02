e73b5ab5285cddaff82e652004f078f32a0a5248
David I. Lehn <dlehn@digitalbazaar.com>
2013-07-02 13:04:18 -0400

    Update to newer spec.
    - This updates to the newer (as of June 2013) HTTP Signatures spec.
      - Use signature key-value pair vs old (and invalid) format.
      - Handle 'request-line' meta-header.
    - Add special case for 'host' header when using 'requests' lib.
    - Use case-insensitive dict for header lookups.
    - Refactor to allow HTTPSignatureAuth to reuse HeaderSigner.
    - Allow secret to be a PEM encoded string.

--- http_signature/requests_auth.py
+++ http_signature/requests_auth.py
@@ -1,10 +1,7 @@
-from wsgiref.handlers import format_date_time
-from datetime import datetime
-from time import mktime
-
 from requests.auth import AuthBase
+from urlparse import urlparse
 
-from .sign import Signer
+from .sign import HeaderSigner
 
 
 class HTTPSignatureAuth(AuthBase):
@@ -17,33 +14,19 @@ class HTTPSignatureAuth(AuthBase):
     algorithm is one of the six specified algorithms
     headers is a list of http headers to be included in the signing string, defaulting to "Date" alone.
     '''
-    def __init__(self, key_id='', secret='', algorithm='rsa-sha256', headers=None, allow_agent=False):
-        self.signer = Signer(secret=secret, algorithm=algorithm, allow_agent=allow_agent)
-        self.key_id = key_id
-        self.headers = headers
-        self.signature_string_head = self.build_header_content()
-    
-    def build_header_content(self):
-        param_map = {'keyId': self.key_id, 
-                     'algorithm': self.signer.algorithm}
-        if self.headers:
-            param_map['headers'] = ' '.join(self.headers)
-        kv = map('{0[0]}="{0[1]}"'.format, param_map.items())
-        kv_string = ','.join(kv)
-        sig_string = 'Signature {0} %s'.format(kv_string)
-        return sig_string
-    
+    def __init__(self, key_id='', secret='', algorithm='rsa-sha256',
+            headers=None, allow_agent=False):
+        self.header_signer = HeaderSigner(key_id=key_id, secret=secret,
+                algorithm=algorithm, headers=headers, allow_agent=allow_agent)
+        self.uses_host = 'host' in [h.lower() for h in headers]
+
     def __call__(self, r):
-        if 'Date' not in r.headers:
-            now = datetime.now()
-            stamp = mktime(now.timetuple())
-            r.headers['Date'] = format_date_time(stamp)
-        if self.headers:
-            signable_list = [r.headers[x] for x in self.headers]
-            signable = '\n'.join(signable_list)
-        else:
-            signable = r.headers['Date']
-        signature = self.signer.sign(signable)
-        r.headers['Authorization'] = self.signature_string_head % signature
+        self.header_signer.sign_headers(
+                r.headers,
+                # 'Host' header unavailable in request object at this point
+                # if 'host' header is needed, extract it from the url
+                host=urlparse(r.url).netloc if self.uses_host else None,
+                method=r.method,
+                path=r.path_url,
+                http_version='1.1')
         return r
-    
--- http_signature/sign.py
+++ http_signature/sign.py
@@ -1,23 +1,24 @@
-from os.path import expanduser
-import base64
-from wsgiref.handlers import format_date_time
 from datetime import datetime
-from time import mktime
 from getpass import getpass
+from os.path import expanduser
+from time import mktime
+from wsgiref.handlers import format_date_time
+import base64
 
 from Crypto.Hash import SHA256, SHA, SHA512, HMAC
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
-from .utils import sig, is_rsa
+from .utils import sig, is_rsa, CaseInsensitiveDict
 
 ALGORITHMS = frozenset(['rsa-sha1', 'rsa-sha256', 'rsa-sha512', 'hmac-sha1', 'hmac-sha256', 'hmac-sha512'])
-HASHES = {'sha256': SHA256,
-          'sha1':   SHA,
+HASHES = {'sha1':   SHA,
+          'sha256': SHA256,
           'sha512': SHA512}
 
 class Signer(object):
-    def __init__(self, secret='~/.ssh/id_rsa', algorithm='rsa-sha256', allow_agent=False):
+    def __init__(self, secret='~/.ssh/id_rsa', algorithm='rsa-sha256',
+            allow_agent=False):
         assert algorithm in ALGORITHMS, "Unknown algorithm"
         self._agent_key = False
         self._rsa = False
@@ -35,8 +36,13 @@ class Signer(object):
                 self._keys = self._keys[1:]
                 self.sign_algorithm, self.hash_algorithm = ('rsa', 'sha1')
         if not self._agent_key and self.sign_algorithm == 'rsa':
-            with open(expanduser(secret)) as fh:
-                k = fh.read()
+            if secret.startswith('-----BEGIN RSA PRIVATE KEY-----'):
+                # string with PEM encoded key data
+                k = secret
+            else:
+                # file with key data
+                with open(expanduser(secret)) as fh:
+                    k = fh.read()
             try:
                 rsa_key = RSA.importKey(k)
             except ValueError:
@@ -83,7 +89,7 @@ class Signer(object):
         if not data:
             raise SystemError('No valid encryption: try allow_agent=False ?')
         return base64.b64encode(data)
-    
+
 
 class HeaderSigner(object):
     '''
@@ -95,35 +101,92 @@ class HeaderSigner(object):
     algorithm is one of the six specified algorithms
     headers is a list of http headers to be included in the signing string, defaulting to "Date" alone.
     '''
-    def __init__(self, key_id='', secret='~/.ssh/id_rsa', algorithm='rsa-sha256',
-            headers=None, allow_agent=False):
-        self.signer = Signer(secret=secret, algorithm=algorithm, allow_agent=allow_agent)
-        self.key_id = key_id
+    def __init__(self, key_id='', secret='~/.ssh/id_rsa',
+            algorithm='rsa-sha256', headers=None, allow_agent=False):
+        self.signer = Signer(secret=secret, algorithm=algorithm,
+                allow_agent=allow_agent)
         self.headers = headers
-        self.signature_string_head = self.build_header_content()
+        self.signature_template = self.build_signature_template(
+                key_id, algorithm, headers)
     
-    def build_header_content(self):
-        param_map = {'keyId': self.key_id, 
-                     'algorithm': self.signer.algorithm}
-        if self.headers:
-            param_map['headers'] = ' '.join(self.headers)
+    def build_signature_template(self, key_id, algorithm, headers):
+        """
+        Build the Signature template for use with the Authorization header.
+
+        key_id is the mandatory label indicating to the server which secret to use
+        algorithm is one of the six specified algorithms
+        headers is a list of http headers to be included in the signing string.
+
+        The signature must be interpolated into the template to get the final Authorization header value.
+        """
+        param_map = {'keyId': key_id, 
+                     'algorithm': algorithm,
+                     'signature': '%s'}
+        if headers:
+            param_map['headers'] = ' '.join(headers)
         kv = map('{0[0]}="{0[1]}"'.format, param_map.items())
         kv_string = ','.join(kv)
-        sig_string = 'Signature {0} %s'.format(kv_string)
+        sig_string = 'Signature {0}'.format(kv_string)
         return sig_string
-    
-    def sign(self, h):
-        header_dict = h.copy()
-        if 'Date' not in header_dict:
+
+    def sign_headers(self, headers, host=None, method=None, path=None,
+            http_version='1.1'):
+        """
+        Add Signature Authorization header to case-insensitive header dict.
+
+        headers is a case-insensitive dict of mutable headers.
+        host is a override for the 'host' header (defaults to value in headers).
+        method is the HTTP method (used for 'request-line').
+        pat is the HTTP path (used for 'request-line').
+        http_version is the HTTP version (used for 'request-line').
+        """
+        if 'date' not in headers:
             now = datetime.now()
             stamp = mktime(now.timetuple())
-            header_dict['Date'] = format_date_time(stamp)
-        if self.headers:
-            signable_list = [header_dict[x] for x in self.headers]
-            signable = '\n'.join(signable_list)
-        else:
-            signable = header_dict['Date']
-        signature = self.signer.sign(signable)
-        header_dict['Authorization'] = self.signature_string_head % signature
-        return header_dict
+            headers['date'] = format_date_time(stamp)
+        required_headers = self.headers or ['date']
+        signable_list = []
+        for h in required_headers:
+            if h == 'request-line':
+                if not method or not path:
+                    raise Exception('method and path arguments required when using "request-line"')
 
+                signable_list.append('%s %s HTTP/%s' %
+                        (method.upper(), path, http_version))
+            elif h == 'host':
+                # 'host' special case due to requests lib restrictions
+                # 'host' is not available when adding auth so must use a param
+                # if no param used, defaults back to the 'host' header
+                if not host:
+                    if 'host' in headers:
+                        host = headers[h]
+                    else:
+                        raise Exception('missing required header "%s"' % (h))
+                signable_list.append('%s: %s' % (h.lower(), host))
+            else:
+                if h not in headers:
+                    raise Exception('missing required header "%s"' % (h))
+
+                signable_list.append('%s: %s' % (h.lower(), headers[h]))
+        signable = '\n'.join(signable_list)
+        signature = self.signer.sign(signable)
+        headers['Authorization'] = self.signature_template % signature
+
+    def sign(self, h, method=None, path=None, http_version='1.1'):
+        """
+        Return a dict of headers with the Authorization header added.
+
+        h is a dict of headers.
+        method is the HTTP method (used for 'request-line').
+        pat is the HTTP path (used for 'request-line').
+        http_version is the HTTP version (used for 'request-line').
+        """
+        # case insensitive copy of headers
+        headers = CaseInsensitiveDict(h)
+        self.sign_headers(
+            headers=headers,
+            method=method,
+            path=path,
+            http_version=http_version
+        )
+        return headers
