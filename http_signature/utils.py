e499215150b617538907bd2775b796242d383822
David I. Lehn <dlehn@digitalbazaar.com>
2013-07-02 12:54:09 -0400

    Add CaseInsensitiveDict.

--- http_signature/utils.py
+++ http_signature/utils.py
@@ -14,3 +14,19 @@ def sig(d):
 
 def is_rsa(keyobj):
     return lkv(keyobj.blob)[0] == "ssh-rsa"
+
+# based on http://stackoverflow.com/a/2082169/151401
+class CaseInsensitiveDict(dict):
+    def __init__(self, d=None, **kwargs):
+        super(CaseInsensitiveDict, self).__init__(**kwargs)
+        if d:
+            self.update((k.lower(), v) for k, v in d.iteritems())
+
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveDict, self).__setitem__(key.lower(), value)
+
+    def __getitem__(self, key):
+        return super(CaseInsensitiveDict, self).__getitem__(key.lower())
+
+    def __contains__(self, key):
+        return super(CaseInsensitiveDict, self).__contains__(key.lower())
